91669628aa3fc13507bd27d579a5e037a8e45d60
rifkiimanudin <rifki.iman@gmail.com>
2020-06-28 13:46:43 +0700

    user

--- application/views/pengguna/index.php
+++ application/views/pengguna/index.php
@@ -17,7 +17,7 @@
 
             <div>
             </div>
-            <table class="table table-hover text-gray-900" id="tabel">
+            <table class="table table-hover text-gray-900" id="example">
                 <thead>
                     <tr>
                         <th scope="col">No</th>
